3be7186a8cccaf8f8eed30602a3dccf321a57266
Ajai <ajaijoseph9882@gmail.com>
2024-10-04 12:36:06 +0530

    Add the files - -02 oct 2024

---
+++ src/Oct/Lab124.java
@@ -0,0 +1,29 @@
+package Oct;
+
+import Oct.ex_02102024.ATBStudent;
+
+public class Lab124 {
+    public static void main(String[] args) {
+        // Object Class - Oops
+        // String Class- ref type
+        // Array
+        // for each
+        // 2nd nested for lop
+
+        // Class -> Blueprint to create something (Objects)
+        // Object -> Instance of a class - created from the class.
+
+        ATBStudent ajai = new ATBStudent();
+        ajai.age  =35;
+
+        ATBStudent joseph = new ATBStudent();
+        joseph.age = 65;
+
+        System.out.println(ajai); // Oct.ex_02102024.ATBStudent@6acbcfc0
+
+        System.out.println(joseph); // Oct.ex_02102024.ATBStudent@5f184fc6
+
+        // abhishek - Ref VARAIBLE - bYTE, ir bits ->  8, 64, null default
+
+    }
+}
---
+++ src/Oct/ex_02102024/ATBStudent.java
@@ -0,0 +1,28 @@
+package Oct.ex_02102024;
+
+public class ATBStudent {
+
+        // C (Class) - ATBStudent
+
+        //  A - Attribute | data members | instance variables | fields | member variables
+        long phone;
+        String email;
+        public int age;
+        int batch8x;
+        String name;
+        boolean isMale;
+        String address;
+        boolean isfeedPaid;
+        String githublink;
+        String sid;
+
+        //  B - Behaviour | methods | member functions
+        void study(){}
+        void eat(){}
+        void walk(){}
+        void sleep(){}
+        void run(){}
+
+       }
+
+
