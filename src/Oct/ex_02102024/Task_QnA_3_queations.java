49cb4a9ce83d526d08662cb2ef1b98da75dcf32f
Ajai <ajaijoseph9882@gmail.com>
2024-10-06 19:25:43 +0530

    Add the files - -02 oct 2024

---
+++ src/Oct/ex_02102024/Task_QnA_3_queations.java
@@ -0,0 +1,36 @@
+package Oct.ex_02102024;
+
+public class Task_QnA_3_queations {
+
+// 1. Write 5 points related to the Class and Objects.
+
+//    1. Class as a Blueprint: A class serves as a blueprint for creating objects. It defines the attributes (fields) and behaviors (methods) that the objects of this class will have.
+//    2. Class does not occupy memory.
+//    3.Class is a group of variables of different data types and a group of methods.
+//    4.A Class in Java can contain:
+//             * Data member
+//             * Method
+//             * Constructor
+//             * Nested Class
+//             * Interface
+
+//    Java Objects
+//    An object in Java is a basic unit of Object-Oriented Programming and represents real-life entities. Objects are the instances of a class that are created to use the attributes and methods of a class.  A typical Java program creates many objects, which as you know, interact by invoking methods. An object consists of :
+//
+//   * State : It is represented by attributes of an object. It also reflects the properties of an object.
+//   * Behavior : It is represented by the methods of an object. It also reflects the response of an object with other objects.
+//   * Identity : It gives a unique name to an object and enables one object to interact with other objects.
+
+
+//   2. OOPs and Features.
+
+//     • Oriented Programming is a methodology or paradigm to design a program using classes and objects.
+//    It simplifies software development and maintenance by providing some concepts (Features):
+//
+//    • Object
+//    • Class
+//    • Inheritance
+//    • Polymorphism
+//    • Abstraction
+//    • Encapsulation
+}
