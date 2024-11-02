794269bfcb6a1e65ffd45f3f288fb970ba780c26
Ajai <ajaijoseph9882@gmail.com>
2024-11-02 20:46:04 +0530

    Add the files - -21 oct 2024

---
+++ src/Oct/ex_Wrapper_exception/Lab171_Exceptions.java
@@ -0,0 +1,19 @@
+package Oct.ex_Wrapper_exception;
+import java.io.FileInputStream;
+
+public class Lab171_Exceptions {
+    public static void main(String[] args) {
+
+        // Checked - JVM knows
+//        FileInputStream file = new FileInputStream("C://a.txt");
+        // JVm knows this that file may not exist
+
+
+        // UnChecked Exception - ArithmeticException
+        int a = 10;
+        int c = a/0;
+        System.out.println(c);
+
+
+
+    }}
